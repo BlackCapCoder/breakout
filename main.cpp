57bc8cc25e474cbf84f8bae4b0f0097d867220d5
BlackCap <mailhph@gmail.com>
2019-03-26 13:43:16 +0100

    input manager

--- main.cpp
+++ main.cpp
@@ -1,7 +1,16 @@
 #include "Game.h"
+#include "InputManager.h"
+
+int keybinds[2 * 3] =
+  { SDLK_h      , MoveLeft
+  , SDLK_l      , MoveRight
+  , SDLK_ESCAPE , Quit
+  };
 
 int main (int argc, const char *argv[]) {
-  Game g {1000, 1000, nullptr};
+  InputManager im = InputManager (keybinds, 3);
+
+  Game g {1000, 1000, &im};
   // while (g.tick());
   g.dispose();
   return 0;
