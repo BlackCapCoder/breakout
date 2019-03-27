529830a0b0c6344945c47af988912444efe7a3a3
AndreasDM <andreasmartila@gmail.com>
2019-03-27 01:08:46 +0100

    pulled imp into Particle.cpp

--- Particle.h
+++ Particle.h
@@ -15,25 +15,11 @@ class Particle : public GameObject {
   double angle, velocity;
   double r=255,g,b;
 
-  public:
+public:
+  Particle (double x, double y, double a, double v) : x{x}, y{y}, angle{a}, velocity{v} {};
 
-    Particle (double x, double y, double a, double v) : x{x}, y{y}, angle{a}, velocity{v} {};
-
-    LogicResult logic (double dt, Game * g)
-    {
-      x += std::sin(angle) * velocity * dt;
-      y += std::cos(angle) * velocity * dt;
-      ttl -= dt;
-
-      return ttl <= 0? Remove : None;
-    }
-
-    void render (SDL_Renderer * rnd)
-    {
-      Rect bounds{x-radius, y-radius, radius*2, radius*2};;
-      SDL_SetRenderDrawColor (rnd, r, g, b, 0);
-      SDL_RenderFillRect     (rnd, bounds.get());
-    }
+  LogicResult logic  (double dt, Game* g);
+  void        render (SDL_Renderer* rnd);
 };
 
 #endif // PARTICLE_H
