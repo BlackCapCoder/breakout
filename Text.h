8a2a2d35eb3c07bdcf8a68a2fd372d7fbd1d4e62
AndreasDM <andreasmartila@gmail.com>
2019-03-30 14:40:32 +0100

    Text class

---
+++ Text.h
@@ -0,0 +1,24 @@
+#ifndef TEXT_H
+#define TEXT_H
+
+#include <string>
+#include <SDL2/SDL.h>
+#include <SDL2/SDL_ttf.h>
+
+#include "GameObject.h"
+
+class Text : public GameObject {
+  SDL_Renderer* r;
+  TTF_Font*     font;
+  SDL_Surface*  surface;
+  SDL_Texture*  texture;
+  SDL_Rect      rect;
+
+public:
+  explicit Text(SDL_Renderer* r, std::string const& text,
+                SDL_Color color, int fontSize, int x, int y);
+
+  void render(SDL_Renderer* r) override;
+};
+
+#endif // TEXT_H
